8bdcc4c199ed77a908bb6de66e5843de88091d6a
Bryan <bryanv.alle23@gmail.com>
2024-11-29 21:38:24 -0300

    .

---
+++ Bank acount.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_bancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //criando conta bancaria
+            conta_bancaria conta = new conta_bancaria("Peppa Poggers da Silva", 10000.00m);
+
+            //Exibindo iformações iniciais
+            Console.WriteLine($"Bem vindo(a). {conta.Titular}");
+
+            conta.exibirsaldo();
+            Console.ReadKey();
+
+            int escolha;
+            do
+            {
+                exibirmenu();
+                escolha = int.Parse(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o Valor de depósito: ");
+                        decimal valordepos = Convert.ToDecimal(Console.ReadLine());
+                        conta.depositar(valordepos);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Valor do saque: ");
+                        decimal valorsaque = Convert.ToDecimal(Console.ReadLine());
+                        conta.saque(valorsaque);
+                        break;
+
+                    case 3:
+                        conta.exibirsaldo();
+                        break;
+
+                    case 4:
+                        Console.Clear();
+                        //Exibindo info iniciais
+                        Console.WriteLine($"Bem vindo(a). {conta.Titular}");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Obrigado por usar o sistema do TRABANK");
+                        break;
+
+                }
+            } while (escolha != 5);
+            
+           
+            
+            Console.ReadKey();
+
+        }
+
+        public static void exibirmenu()
+        {
+            Console.WriteLine("\nEscolha uma opção");
+            Console.WriteLine(" 1 - Depositar");
+            Console.WriteLine(" 2 - Sacar");
+            Console.WriteLine(" 3 - Exibir Saldo");
+            Console.WriteLine(" 4 - Reiniciar");
+            Console.WriteLine(" 5 - Sair");
+        }
+
+    }
+}
--- Lista.cs
+++ Lista.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
 
 namespace lista_de_tarefas
 {
@@ -10,16 +12,45 @@ namespace lista_de_tarefas
     {
         static void Main(string[] args)
         {
-            List<string> tarefas = new List<string>();
-            int opcao;
+            // caminho para o banco de dados
+            string dbfile = "tarefas.db";
 
+            //string de conexão com o banco de dados
+            string connectionString = $"data source={dbfile}: version = 3;";
+
+            //Verifica se o arquivo do db ja existe; se não, cria o db e tabela
+            if (!File.Exists(dbfile)) 
+            {
+                //cria novo arquivo
+                SQLiteConnection.CreateFile(dbfile);
+
+                //criar tabela no banco de dados
+                using (SQLiteConnection con = new SQLiteConnection(connectionString)) 
+                {
+                    con.Open();
+
+                    //comando sql para criar tabea tarefas
+                    string sql = "CREATE TABLE tarefas (id INTENGER PRIMARY KEY AUTOINCREMENT, Descricao TEXT NOT NULL)";
+
+                    using (SQLiteCommand cmd = SQLiteCommand(sql, con))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    con.Close();
+                }
+            }
+            //List<string> tarefas = new List<string>();
+            int opcao; 
+            
             do //faça
             {
                 Console.WriteLine("\n Lista de Tarefas");
                 Console.WriteLine("1 - Adicionar Tarefa");
                 Console.WriteLine("2 - Remover Tarefa");
                 Console.WriteLine("3 - Exibir Tarefas");
-                Console.WriteLine("4 - Sair");
+                Console.WriteLine("4 - Atualiar");
+                Console.WriteLine("5 - Limpar Console");
+                Console.WriteLine("6 - Sair");
                 opcao = Convert.ToInt32(Console.ReadLine());
 
                 switch (opcao)
@@ -27,14 +58,34 @@ namespace lista_de_tarefas
                     case 1:
                         Console.WriteLine("Digite a nova tarefa:");
                         string novaTarefa = Console.ReadLine();
-                        tarefas.Add(novaTarefa);
+                        //tarefas.Add(novaTarefa);
+
+                        //Insere a nova tarefa do db
+                        using (SQLiteConnection con = new SQLiteConnection(connectionString))
+                        {
+                            con.Open();
+
+                            //Comando SQL para inserir a nova tarefa
+                            string sql_insert = "INSERT INTO Tarefas (Descricao) " + "VALUES (@Descricao)";
+                            using (SQLiteCommand cmd = new SQLiteCommand(sql_insert, con))
+                            {
+                                //Adiciona o parâmetro @Descricao com o valor da nova tarefa
+                                cmd.Parameters.AddWithValue("@Descricao", novaTarefa);
+                                cmd.ExecuteNonQuery(); //Executa o comando SQL
+                            }
+
+                            con.Close();
+                        }
                         Console.WriteLine("Sucesso! Tarefa adicionada");
                         break;
 
                     case 2:
                         Console.WriteLine("Digite a tarefa a ser removida:");
                         string tarefaRemover = Console.ReadLine();
-                        if (tarefas.Remove(tarefaRemover))
+                        
+                        // Remova tarefa do bd com base no ID
+
+                        if (rowsAffected > 0)
                         {
                             Console.WriteLine("Tarefa removida com sucesso!");
                         }
@@ -46,16 +97,45 @@ namespace lista_de_tarefas
                     case 3:
                         Console.WriteLine("Lista de tarefas:\n");
 
-                        foreach (string tarefa in tarefas)
+                        //Selecionar todas as tarefas
+                        using (SQLiteConnection con = new SQLiteConnection(connectionString))
                         {
-                            Console.WriteLine(tarefa);
+                            con.Open();
+                            //
+                            string sql_select = "SSELECT * FROM tarefas";
+
+                            using (SQLiteCommand cmd = new SQLiteCommand(sql_select, con))
+                            {
+                                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                                {
+                                    //Lê cada registro retornando
+                                    while(reader.Read())
+                                    {
+                                        //Exibe o ID e a descrição da tarefa
+                                        Console.WriteLine($"ID: {reader["id"]} - Descrição:{reader["Descricao"]}");
+                                    }
+                                }
+                            }
+                                con.Close();
                         }
-                        break;
+
+                            //foreach (string tarefa in tarefas)
+                            //{
+                            //    Console.WriteLine(tarefa);
+                            //}
+                            break;
                     case 4:
-                        Console.WriteLine("Encerrando...");
+                    //    Console.WriteLine("Encerrando...");
+                    //    break;
+                    //default:
+                    //    Console.WriteLine("Opção inválida");
+                        
                         break;
-                    default:
-                        Console.WriteLine("Opção inválida");
+                        case 5:
+                        Console.Clear();
+                        break;
+                        case 6:
+                        Console.WriteLine("Encerrando...");
                         break;
                 }
 
